1d191888176dac8f953b44722e7b432db13209e8
Kembo <asininefatuity@gmail.com>
2024-08-29 15:45:11 +0300

    corrected postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,8 @@
 import fse from 'fs-extra';
 import path from 'path';
-const topDir = import.meta.dirname;
+
+// Get the directory name of the current module
+const topDir = new URL('.', import.meta.url).pathname;
+
 fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
 fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
